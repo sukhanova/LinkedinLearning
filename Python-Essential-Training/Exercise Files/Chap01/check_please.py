27bc22e2b5faa714a3abe19989cc230a75f455ff
sukhanova <verusia.s@gmail.com>
2020-10-01 20:52:32 -0500

    added solution for function-challenge Chap02

--- Python-Essential-Training/Exercise Files/Chap01/check_please.py
+++ Python-Essential-Training/Exercise Files/Chap01/check_please.py
@@ -4,10 +4,10 @@ import sys
 
 print('Hello, World.')
 
-
+MASTER_PASSWORD = 'password'
 password = input("Please enter the super secret password: ")
 attempt_count = 1
-while password != 'password':
+while password != MASTER_PASSWORD:
   if attempt_count > 3:
    sys.exit("Too many invalid password attempts") 
   password = input("Invalid password, please try again: ")
